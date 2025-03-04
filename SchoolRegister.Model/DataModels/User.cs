59081d46fdd40c67120be03156f11b3f272e7de4
Rafal Grycuk <smth@smth.smth>
2025-03-04 10:10:46 +0100

    Lab 1 start

---
+++ SchoolRegister.Model/DataModels/User.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+namespace SchoolRegister.Model.DataModels;
+public class User : IdentityUser<int>
+{
+    public required string FirstName { get; set; }
+    public string LastName { get; set; } = null!;
+     public DateTime RegistrationDate { get; set; } = DateTime.Now;
+}

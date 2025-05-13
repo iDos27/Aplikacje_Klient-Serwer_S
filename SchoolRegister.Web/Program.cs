71479a84d4e9c2f1fe2813ebda134459d72dc794
Piotr Janiszewski <idos2772@gmail.com>
2025-05-13 08:38:50 +0200

    Poprawa connectionStringa

--- SchoolRegister.Web/Program.cs
+++ SchoolRegister.Web/Program.cs
@@ -5,19 +5,26 @@ using SchoolRegister.Model.DataModels;
 using SchoolRegister.Services.Configuration.AutoMapperProfiles;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddAutoMapper(typeof(MainProfile));
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
-.AddRoles<Role>()
-.AddRoleManager<RoleManager<Role>>()
-.AddUserManager<UserManager<User>>()
-.AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddTransient(typeof(ILogger), typeof(Logger<Program>));
-builder.Services.AddControllersWithViews();
+// Add services to the container
+var connectionString = builder.Configuration.GetConnectionString ("DefaultConnection");
+builder.Services.AddAutoMapper (typeof (MainProfile));
+builder.Services.AddDbContext<ApplicationDbContext> (options =>
+options.UseSqlServer (connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter ();
+builder.Services.AddDefaultIdentity<User> (options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<Role> ()
+    .AddRoleManager<RoleManager<Role>> ()
+    .AddUserManager<UserManager<User>> ()
+    .AddEntityFrameworkStores<ApplicationDbContext> ();
+builder.Services.AddTransient (typeof (ILogger), typeof (Logger<Program>));
+builder.Services.AddScoped<IStringLocalizer, StringLocalizer<BaseController>> ();
+builder.Services.AddScoped<ISubjectService, SubjectService> ();
+builder.Services.AddScoped<IGradeService, GradeService> ();
+builder.Services.AddScoped<IGroupService, GroupService> ();
+builder.Services.AddScoped<IStudentService, StudentService> ();
+builder.Services.AddScoped<ITeacherService, TeacherService> ();
+
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
